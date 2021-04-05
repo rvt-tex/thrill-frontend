991d603ae686976d9494fa869dc28efcaed8abab
github username <rvtingling@yahoo.com>
2021-04-05 18:31:05 -0400

    added font awesome to index.html, added menu icon in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,9 @@ function Navbar() {
                 <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                     THRILL
                 </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
                 </div>
             </nav>
         </>
