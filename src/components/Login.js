0f457a0d0152f249521f1d46c3145fc2f541f7dd
github username <rvtingling@yahoo.com>
2021-04-04 21:52:05 -0400

    created login and profile component

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+    return (
+      <>
+       <h1>Welcome to Five Star Tours Loin Page</h1>
+      </>
+    );
+}
+  
+export default Login;
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Profile() {
+    return (
+      <>
+       <h1>Welcome to Five Star Tours Profile Page </h1>
+      </>
+    );
+}
+  
+export default Profile;
