791a03db3bb0f65f3b29bc7bc17ebdda6abe3047
github username <rvtingling@yahoo.com>
2021-04-05 20:57:41 -0400

    update navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,12 +15,32 @@ function Navbar() {
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                    THRILL
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
+                        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                            THRILL
+                        </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/tours' className='nav-links' onClick={closeMobileMenu}>Tours</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/Galleries' className='nav-links' onClick={closeMobileMenu}>Galleries</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/Reservations' className='nav-links' onClick={closeMobileMenu}>Reservations</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/Reviews' className='nav-links' onClick={closeMobileMenu}>Reviews</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/Profile' className='nav-links' onClick={closeMobileMenu}>Profile</Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
